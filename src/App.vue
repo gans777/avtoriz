6580d67f93b996a2d01e184f30d7306efd6d6f88
gans777 <animekogan@gmail.com>
2021-01-24 02:46:30 +0300

    first commit avtoriz

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AvtorizStart />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AvtorizStart from "./components/AvtorizStart.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AvtorizStart
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/AvtorizStart.vue
@@ -0,0 +1,48 @@
+<template>
+
+	<div class="mt-5">
+		<b-container  fluid  class="avtoriz d-flex justify-content-center" >
+			<b-row>
+				<b-col>
+  
+  <b-button variant="outline-primary" v-on:click="registration()">регистрация</b-button>
+  
+</b-col>
+<b-col><b-button variant="outline-primary" v-on:click="Avtorization()">авторизация</b-button></b-col>
+</b-row>
+</b-container > 
+<b-container>
+<b-row v-if="reg_form">
+	<b-col>
+	<b-form-input  placeholder="Enter your name"></b-form-input>
+	<b-form-input  placeholder="Enter your password"></b-form-input>
+	<b-button variant="outline-primary" v-on:click="save_registration()">save</b-button>
+	</b-col>
+</b-row>
+</b-container > 
+</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+				reg_form: false
+			}
+		},
+		methods: {Avtorization()
+      { this.reg_form=false
+  },
+  registration(){this.reg_form=true;
+     },
+    save_registration(){  	console.log("click save_registration");
+    } 
+  }
+	}
+</script>
+
+<style>
+.avtoriz {
+
+}
+</style>
